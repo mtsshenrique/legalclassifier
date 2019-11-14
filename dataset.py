ab3b580c4a00e2c9a03b21e6f24f77c84b8e55e1
Matheus Lopes <matheus_henriquee@hotmail.com>
2019-11-13 23:21:55 -0300

    Add files via upload
    captura apenas as pubs com o termo "recuperacao judicial"

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ from pandas import DataFrame
 import unidecode
 import os
 
+import re
+
 id=0
 dict = {}
 arqs_lidos = 0
@@ -42,39 +44,46 @@ for root, dirs, files in os.walk("publicacoes"):
                     pubs_lidas+=1
                     i+=1
                     categoria = 0
-                    if 'falencia' in pub or 'falida' in pub or 'falido' in pub:
+
+                    if re.search(r'recuperacao judicial', pub):
+                        id+=1
+                        print (id)
+                        dict[pub] = categoria
+                        continue
+                    else:
+                        continue
+
+                    #if 'falencia' in pub or 'falida' in pub or 'falido' in pub:
                         #if (('decretacao de falencia' in pub or 'decretada a falencia' in pub) and 'encerramento' in pub):
-                        if (('decretacao de falencia' in pub or 'decretada a falencia' in pub)):
-                            categoria = 1
-                            #feito
-                        #if 'convolacao da recuperacao judicial em falencia' in pub and ('convolo' in pub or 'relacao de credores' in pub):
-                        if 'convolacao da recuperacao judicial em falencia' in pub:
-                            categoria = 2
-                            #feito
-                        #if 'extensao dos efeitos da falencia' in pub and 'recuperacao judicial' in pub:
-                        if 'extensao dos efeitos da falencia' in pub:
-                            categoria = 3
+                    if (('decretacao de falencia' in pub or 'decretada a falencia' in pub)):
+                        categoria = 1
+                        #feito
+                    #if 'convolacao da recuperacao judicial em falencia' in pub and ('convolo' in pub or 'relacao de credores' in pub):
+                    if 'convolacao da recuperacao judicial em falencia' in pub:
+                        categoria = 2
+                        #feito
+                    #if 'extensao dos efeitos da falencia' in pub and 'recuperacao judicial' in pub:
+                    if 'extensao dos efeitos da falencia' in pub:
+                        categoria = 3
+                        #feito
+
+                    #if 'insolvencia' in pub:
+                    if ('declaro a insolvencia' in pub or 'declaracao de insolvencia' in pub):
+                       categoria = 4
                             #feito
 
-                    if 'insolvencia' in pub:
-                        if ('declaro a insolvencia' in pub or 'declaracao de insolvencia' in pub):
-                            categoria = 4
-                            #feito
+                    if 'decretada a liquidacao extrajudicial' in pub:
+                        categoria = 5
 
-                    if 'recuperacao extrajudicial' in pub or 'liquidacao extrajudicial' in pub:
-                        #if 'decretada a liquidacao extrajudicial' in pub:
-                        if 'decretad' in pub:
-                            categoria = 5
-
-                    if 'recuperacao judicial' in pub:
+                    #if 'recuperacao judicial' in pub:
                         #if 'processamento da recuperacao judicial' in pub and ('concedido o processamento' in pub or 'plano de recuperacao' in pub):
-                        if 'processamento da recuperacao judicial' in pub:
-                            categoria = 6
+                    if 'processamento da recuperacao judicial' in pub:
+                       categoria = 6
 
                     if categoria != 0:
-                        id+=1
+                        id+=0
                         dict[pub] = categoria
-                        print ("\n\nPUB =>"+i.__str__()+"\nCATEGORIA IDENTIFICADA => "+categoria.__str__())
+                        #print (id.__str__()+"\n\nPUB =>"+i.__str__()+"\nCATEGORIA IDENTIFICADA => "+categoria.__str__())
                         if (id==-300):
                             break
             if id==-300:
@@ -88,4 +97,4 @@ print("Arquivos lidos {0}".format(arqs_lidos))
 print("Pubs lidas {0}".format(pubs_lidas))
 df = DataFrame(list(dict.items()), columns=['pub','categoria'])
 print(df.categoria.value_counts())
-export_csv = df.to_csv (r'export_dataframe.csv', index = None, header=True)
+export_csv = df.to_csv (r'export_dataframe_13112019.csv', index = None, header=True)
